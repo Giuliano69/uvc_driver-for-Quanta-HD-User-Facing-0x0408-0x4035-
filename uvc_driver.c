61126daec7a19781da85af6fcfeb2d1914b2b273
Giuliano69 <giuliano.lotta@gmail.com>
2023-05-25 00:27:45 +0200

    Update uvc_driver.c
    Added experimental for Quanta id 0408:4033

--- uvc_driver.c
+++ uvc_driver.c
@@ -2669,19 +2669,7 @@ static const struct uvc_device_info uvc_quirk_force_y8 = {
  * though they are compliant.
  */
 static const struct usb_device_id uvc_ids[] = {
-	/* Quanta ACER HD User Facing 0408:4033*/
-	{ .match_flags 	= USB_DEVICE_ID_MATCH_DEVICE
-			| USB_DEVICE_ID_MATCH_INT_INFO,
-	  .idVendor = 0x0408,
-	  .idProduct = 0x4033,
-	  .bInterfaceClass = USB_CLASS_VIDEO,
-	  .bInterfaceSubClass = 1,
-	  .bInterfaceProtocol =	UVC_PC_PROTOCOL_15,
-	  .driver_info = (kernel_ulong_t) &(const struct uvc_device_info ) {
-										.uvc_version = 0x010a, } },
-
-	
-	/* Quanta ACER HD User Facing 0408:4035*/
+	/* Quanta ACER HD User Facing */
 	{ .match_flags 	= USB_DEVICE_ID_MATCH_DEVICE
 			| USB_DEVICE_ID_MATCH_INT_INFO,
 	  .idVendor = 0x0408,
@@ -2691,6 +2679,18 @@ static const struct usb_device_id uvc_ids[] = {
 	  .bInterfaceProtocol =	UVC_PC_PROTOCOL_15,
 	  .driver_info = (kernel_ulong_t) &(const struct uvc_device_info ) {
 										.uvc_version = 0x010a, } },
+										
+	/* Quanta ACER HD User Facing 4033 - Experimental !! */
+	{ .match_flags 	= USB_DEVICE_ID_MATCH_DEVICE
+			| USB_DEVICE_ID_MATCH_INT_INFO,
+	  .idVendor = 0x0408,
+	  .idProduct = 0x4033,
+	  .bInterfaceClass = USB_CLASS_VIDEO,
+	  .bInterfaceSubClass = 1,
+	  .bInterfaceProtocol =	UVC_PC_PROTOCOL_15,
+	  .driver_info = (kernel_ulong_t) &(const struct uvc_device_info ) {
+										.uvc_version = 0x010a, } },
+										
 
 	/* LogiLink Wireless Webcam */
 	{ .match_flags		= USB_DEVICE_ID_MATCH_DEVICE
@@ -3236,3 +3236,4 @@ MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
 MODULE_VERSION(DRIVER_VERSION);
+
